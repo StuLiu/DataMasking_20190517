f258eae8ed1930a77de71c6a9f406f5e3f6b8059
liuwang20144623 <1183862787@qq.com>
2019-05-18 09:27:29 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+--------------------------------------------------------
+@File    :   main.py.py    
+@Contact :   1183862787@qq.com
+@License :   (C)Copyright 2017-2018, CS, WHU
+
+@Modify Time : 2019/5/18 1:01     
+@Author      : Liu Wang    
+@Version     : 1.0   
+@Desciption  : None
+--------------------------------------------------------  
+''' 
+
+import sys
+import main_window
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = main_window.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
