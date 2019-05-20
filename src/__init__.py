e431cef00816f5ff2acb64fd4f6e8013dfc7ae5a
liuwang20144623 <1183862787@qq.com>
2019-05-20 22:09:29 +0800

    添加数据文件模拟

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+'''
+--------------------------------------------------------
+@File    :   __init__.py.py    
+@Contact :   1183862787@qq.com
+@License :   (C)Copyright 2017-2018, CS, WHU
+
+@Modify Time : 2019/5/20 22:05     
+@Author      : Liu Wang    
+@Version     : 1.0   
+@Desciption  : None
+--------------------------------------------------------  
+''' 
